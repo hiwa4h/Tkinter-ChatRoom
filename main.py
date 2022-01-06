bfc427f6c2b5cab8c77f67f048f349154b21bd60
hiwa4h <72159083+hiwa4h@users.noreply.github.com>
2022-01-06 14:49:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,10 @@ from ttkthemes import themed_tk
 def Start_app():
     import app
 
-def setting():
-    import setting
 
 root = themed_tk.ThemedTk(theme='arc')
 
 b1 = ttk.Button(root, text='Start', command=Start_app)
 b1.place(x=50,y=20)
-
-b2 = ttk.Button(root, text='Settings', command=setting)
-b2.place(x=50, y=60)
 
 root.mainloop()

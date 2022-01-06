b075df64ec7978fbe1e0365f697b5d8727ec03cd
JazzTGB <91386746+Jaasim2008@users.noreply.github.com>
2022-01-06 22:35:23 +0400

    Add files via upload

---
+++ Clients/client01.py
@@ -0,0 +1,19 @@
+import socket
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 6969
+
+username = input('Enter a Username : ')
+
+while True:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+
+
+    def send_to_server():
+        s.send(full_message.encode('utf-8'))
+
+
+    msg = input('Enter Your Message Here : ')
+    full_message = f'[  {username}  ] -->  {msg}'
+    send_to_server()
---
+++ Server/server.py
@@ -0,0 +1,18 @@
+import socket
+import datetime
+
+now = datetime.datetime.now()
+
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 6969
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(5)
+
+print(f'Server has been Started at {now.strftime("%S:%M:%H %y-%m-%d")}')
+
+while True:
+    comms, address = s.accept()
+    msg = comms.recv(4096)
+    print(msg.decode('utf-8'))

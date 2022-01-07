35dde63f3f04ca177809210e2956c68435106cc7
JazzTGB <91386746+Jaasim2008@users.noreply.github.com>
2022-01-07 13:42:24 +0400

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -12,9 +12,17 @@ s.listen(5)
 
 print(f'Server has been Started at {now.strftime("%S:%M:%H %y-%m-%d")}')
 
+present_clients = []
+collected_ip = False
+ip_address_list = []
+
 while True:
     comms, address = s.accept()
+    if not collected_ip:
+        ip_address = comms.recv(4096)
+        print(ip_address.decode('utf-8'))
+        ip_address_list.append(ip_address.decode('utf-8'))
+        print(f'{ip_address_list[0]} has connected to the Server!')
+        collected_ip = True
     msg = comms.recv(4096)
     print(msg.decode('utf-8'))
-
-    
